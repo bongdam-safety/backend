336a909a0d66ebca9e5b22bd28fb24a5ed574807
mgkim1 <mgkim1209@outlook.com>
2024-10-07 17:51:24 +0900

    열 추가(분류설명, 생성일, 수정일)

--- src/main/java/kr/co/bongdamsafety/onlinemap/dto/FacilityCategoryForm.java
+++ src/main/java/kr/co/bongdamsafety/onlinemap/dto/FacilityCategoryForm.java
@@ -1,18 +1,24 @@
 package kr.co.bongdamsafety.onlinemap.dto;
 
-import jakarta.persistence.Column;
 import kr.co.bongdamsafety.onlinemap.entity.FacilityCategory;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.sql.Timestamp;
+
+@NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class FacilityCategoryForm {
     private Long id;
     private String categoryName;
     private boolean visible;
+    private String note;
+    private Timestamp date_created;
+    private Timestamp date_edited;
 
     public FacilityCategory toEntity() {
-        return new FacilityCategory(id, categoryName, visible);
+        return new FacilityCategory(id, categoryName, visible, note, date_created, date_edited);
     }
 }
