13da576220403d509e5d6cb4e249b1e54bc61aab
mgkim1 <mgkim1209@outlook.com>
2024-10-08 04:02:17 +0900

    main page - hello world

---
+++ src/main/java/kr/co/bongdamsafety/onlinemap/api/IndexController.java
@@ -0,0 +1,15 @@
+package kr.co.bongdamsafety.onlinemap.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class IndexController {
+    // GET
+    @GetMapping("/")
+    public String index() {
+        return "Hello world!";
+    }
+}
