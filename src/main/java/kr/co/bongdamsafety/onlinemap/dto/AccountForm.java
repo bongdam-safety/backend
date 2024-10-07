744adf348963e840b49319ba6a3aeafb59cbe9a2
mgkim1 <mgkim1209@outlook.com>
2024-10-07 17:50:21 +0900

    열 추가
    권한레벨, 계정상세정보, 계정정보수정일, 계정생성자

--- src/main/java/kr/co/bongdamsafety/onlinemap/dto/AccountForm.java
+++ src/main/java/kr/co/bongdamsafety/onlinemap/dto/AccountForm.java
@@ -1,7 +1,7 @@
 package kr.co.bongdamsafety.onlinemap.dto;
 
-import jakarta.persistence.Column;
 import kr.co.bongdamsafety.onlinemap.entity.Account;
+import kr.co.bongdamsafety.onlinemap.repository.AccountRepository;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 
@@ -13,9 +13,19 @@ public class AccountForm {
     private String id;
     private String password;
     private String name;
+    private int permlevel;
+    private String content;
     private Timestamp date_created;
+    private Timestamp date_edited;
+    private String account_id_created_this;
 
-    public Account toEntity(){
-        return new Account(id, password, name, date_created);
+
+
+    public Account toEntity(AccountRepository accountRepository) {
+
+        Account account_created_this = accountRepository.findById(account_id_created_this)
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 수정계정입니다: " + account_id_created_this));
+
+        return new Account(id, password, name, permlevel, content, date_created, date_edited, account_created_this);
     }
 }
