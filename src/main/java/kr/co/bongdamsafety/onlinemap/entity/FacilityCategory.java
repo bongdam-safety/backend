115a4e030f11fc10a8a9f6357be2e6e026f2f955
mgkim1 <mgkim1209@outlook.com>
2024-10-07 17:56:01 +0900

    분류설명, 생성 및 수정일 추가

--- src/main/java/kr/co/bongdamsafety/onlinemap/entity/FacilityCategory.java
+++ src/main/java/kr/co/bongdamsafety/onlinemap/entity/FacilityCategory.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.sql.Timestamp;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
@@ -16,10 +18,16 @@ public class FacilityCategory {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "facilitycategory_seq_gen")
     @SequenceGenerator(name = "facilitycategory_seq_gen", sequenceName = "facilitycategory_seq", allocationSize = 1)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false, length = 4000)
     private String categoryName;
     @Column(nullable = false)
     private boolean visible;
+    @Column(length = 4000)
+    private String note;
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp date_created;
+    @Column
+    private Timestamp date_edited;
 
     public void patch(FacilityCategory category) {
         this.categoryName = category.categoryName;
