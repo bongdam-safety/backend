f2d93e14c76f3c32b494eb35226bf8b6434eb2d2
mgkim1 <mgkim1209@outlook.com>
2024-10-16 14:30:16 +0900

    api 개발(정보수정요청, 지도에 장소(마커)추가요청, 센터에 요청)
    센터에 요청 : 주민자치회에 요청하는 것으로, 나중에 자치회에서 봉담읍 안전거리 조성 사업을 할 때, 이 의견을 안전시설물 설치 등을 하는 데 있어 활용할 것입니다.

---
+++ src/main/java/kr/co/bongdamsafety/onlinemap/api/Request_ToCenterController.java
@@ -0,0 +1,36 @@
+package kr.co.bongdamsafety.onlinemap.api;
+
+import kr.co.bongdamsafety.onlinemap.dto.Request_ToCenterForm;
+import kr.co.bongdamsafety.onlinemap.entity.Request_ToCenter;
+import kr.co.bongdamsafety.onlinemap.repository.Request_ToCenterRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class Request_ToCenterController {
+    @Autowired
+    Request_ToCenterRepository request_ToCenterRepository;
+
+    // GET **** 로그인되고 승인된 관리자만 조회가능하도록 해야함 ****
+    @GetMapping("/api/request_ToCenter")
+    public List<Request_ToCenter> index(){
+        return request_ToCenterRepository.findAll();
+    }
+
+    // GET - 단일 센터에 요청 **** 로그인되고 승인된 관리자만 조회가능하도록 해야함 ****
+    @GetMapping("/api/request_ToCenter/{id}")
+    public Request_ToCenter show(@PathVariable Long id){
+        return request_ToCenterRepository.findById(id).orElse(null);
+    }
+
+    // POST
+    @PostMapping("api/request_ToCenter")
+    public Request_ToCenter create(@RequestBody Request_ToCenterForm dto) {
+        Request_ToCenter request_ToCenter = dto.toEntity();
+        return request_ToCenterRepository.save(request_ToCenter);
+    }
+}
