b516dceaf248a206f1d5a8bede33e16a7ebd7a7f
mgkim1 <mgkim1209@outlook.com>
2024-10-30 01:02:59 +0900

    시설물분류 id를 기반으로 시설물 정보 불러오기

--- src/main/java/kr/co/bongdamsafety/onlinemap/service/FacilityService.java
+++ src/main/java/kr/co/bongdamsafety/onlinemap/service/FacilityService.java
@@ -3,6 +3,7 @@ package kr.co.bongdamsafety.onlinemap.service;
 import jakarta.annotation.PostConstruct;
 import kr.co.bongdamsafety.onlinemap.dto.FacilityForm;
 import kr.co.bongdamsafety.onlinemap.entity.Facility;
+import kr.co.bongdamsafety.onlinemap.entity.FacilityCategory;
 import kr.co.bongdamsafety.onlinemap.repository.AccountRepository;
 import kr.co.bongdamsafety.onlinemap.repository.FacilityCategoryRepository;
 import kr.co.bongdamsafety.onlinemap.repository.FacilityRepository;
@@ -32,6 +33,14 @@ public class FacilityService {
 
     public Facility findById(Long id) {
         return facilityRepository.findById(id).orElse(null); // id를 받아 해당하는 시설물 정보 불러오기
+    }
+
+    public List<Facility> findByCategory(Long id) { // 카테고리 아이디로 해당 카테고리에 해당하는 시설물 정보 불러오기
+        FacilityCategory facilityCategory = facilityCategoryRepository.findById(id).orElse(null);
+        if(facilityCategory != null) {
+            return facilityRepository.findByFacilityCategory(facilityCategory); // 카테고리에 해당하는 시설물 목록 반환
+        }
+        return new ArrayList<>(); // 없는 카테고리일 경우 빈 리스트 리턴
     }
 
     private static final String UPLOAD_DIR = "D:/github/backend/src/main/resources/static/upload/images";
