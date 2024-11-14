76f5709f5086b2adee97b800e4cc9dc47049bce4
mgkim1 <mgkim1209@outlook.com>
2024-11-15 02:37:54 +0900

    crud - update, delete 추가

--- src/main/java/kr/co/bongdamsafety/onlinemap/service/FacilityService.java
+++ src/main/java/kr/co/bongdamsafety/onlinemap/service/FacilityService.java
@@ -10,6 +10,8 @@ import kr.co.bongdamsafety.onlinemap.repository.FacilityCategoryRepository;
 import kr.co.bongdamsafety.onlinemap.repository.FacilityRepository;
 import kr.co.bongdamsafety.onlinemap.repository.Request_NewToMapRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -41,7 +43,7 @@ public class FacilityService {
 
     public List<Facility> findByCategory(Long id) { // 카테고리 아이디로 해당 카테고리에 해당하는 시설물 정보 불러오기
         FacilityCategory facilityCategory = facilityCategoryRepository.findById(id).orElse(null);
-        if(facilityCategory != null) {
+        if (facilityCategory != null) {
             return facilityRepository.findByFacilityCategory(facilityCategory); // 카테고리에 해당하는 시설물 목록 반환
         }
         return new ArrayList<>(); // 없는 카테고리일 경우 빈 리스트 리턴
@@ -63,7 +65,7 @@ public class FacilityService {
 
         List<String> imageUrls = new ArrayList<>(); // 이미지 주소들이 들어가는 리스트
         for (MultipartFile file : dto.getImages()) { // 들어온 이미지파일들이 하나씩 돌아가면서 반복
-            if(!file.isEmpty()) {
+            if (!file.isEmpty()) {
                 String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename(); // 현재시간과 기존 파일명을 합쳐 새 파일명 제작
                 File dest = new File(UPLOAD_DIR + "/" + fileName); // 파일경로
                 try {
@@ -80,7 +82,7 @@ public class FacilityService {
 
     public Facility createByNewToMapId(Long newToMapId) { // 신규요청번호를 기반으로, 지도에 새 마커 생성
         Request_NewToMap request_newToMap = request_NewToMapRepository.findById(newToMapId).orElse(null); // 매개변수로 들어온 신규시설물추가요청 id를 토대로 신규요청 데이터 가져오기
-        if(request_newToMap == null) {
+        if (request_newToMap == null) {
             return null; // 해당하는 요청번호가 존재하지 않을 경우 null
         }
         // 아래에 있는 것들은, 신규 시설물 요청 정보를 빼와서, 시설물 객체에 넣어주는 과정입니다.
@@ -96,5 +98,30 @@ public class FacilityService {
         request_NewToMapRepository.delete(request_newToMap); // 요청 삭제(매개변수로는 엔티티 객체를 넣어줘야 함!!)
 
         return facilityRepository.save(facility); // 시설물 저장
+    }
+
+    public ResponseEntity<Facility> update(Long id, FacilityForm dto) { // 시설물 정보 수정
+        Facility facility = dto.toEntity(facilityCategoryRepository, accountRepository_new, accountRepository_edit);
+
+        Facility target = facilityRepository.findById(id).orElse(null);
+
+        if (target == null || id != facility.getId()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        target.patch(facility);
+        Facility updated = facilityRepository.save(target);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+
+    public ResponseEntity<Facility> delete(Long id) {
+        Facility target = facilityRepository.findById(id).orElse(null);
+
+        if (target == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        facilityRepository.delete(target);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
