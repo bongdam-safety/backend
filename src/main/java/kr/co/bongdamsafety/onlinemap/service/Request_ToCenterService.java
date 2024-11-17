31ffdcb2b762c1e5278e0dacc8c6433a5a02bfb4
김명규 <mgkim1209@outlook.com>
2024-11-17 23:57:29 +0900

    파일 데이터가 안들어와도 업로드 가능하게 설정

--- src/main/java/kr/co/bongdamsafety/onlinemap/service/Request_ToCenterService.java
+++ src/main/java/kr/co/bongdamsafety/onlinemap/service/Request_ToCenterService.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class Request_ToCenterService {
@@ -28,7 +29,7 @@ public class Request_ToCenterService {
         return request_ToCenterRepository.findById(id).orElse(null); // id를 받아 해당하는 요청정보 불러오기
     }
 
-    private static final String UPLOAD_DIR = "D:/github/backend/src/main/resources/static/upload/tocenter_images";
+    private static final String UPLOAD_DIR = "C:/websites/bongdamsafe1/src/main/resources/static/upload/tocenter_images";
     private static final String RELATIVE_UPLOAD_DIR = "/static/upload/tocenter_images";
 
     @PostConstruct
@@ -43,18 +44,21 @@ public class Request_ToCenterService {
         Request_ToCenter request_ToCenter = dto.toEntity();
 
         List<String> imageUrls = new ArrayList<>(); // 이미지 주소들이 들어가는 리스트
-        for (MultipartFile file : dto.getImages()) { // 들어온 이미지파일들이 하나씩 돌아가면서 반복
-            if(!file.isEmpty()) {
-                String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename(); // 현재시간과 기존 파일명을 합쳐 새 파일명 제작
-                File dest = new File(UPLOAD_DIR + "/" + fileName); // 파일경로
-                try {
-                    file.transferTo(dest); // 파일을 경로에 그 파일명으로 저장
-                    imageUrls.add(RELATIVE_UPLOAD_DIR + "/" + fileName); // 리스트에 파일 상대주소 추가
-                } catch (IOException e) {
-                    e.printStackTrace(); // 에러 발생시 콘솔에 출력
+        if (Objects.nonNull(dto.getImages())){
+            for (MultipartFile file : dto.getImages()) { // 들어온 이미지파일들이 하나씩 돌아가면서 반복
+                if(!file.isEmpty()) {
+                    String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename(); // 현재시간과 기존 파일명을 합쳐 새 파일명 제작
+                    File dest = new File(UPLOAD_DIR + "/" + fileName); // 파일경로
+                    try {
+                        file.transferTo(dest); // 파일을 경로에 그 파일명으로 저장
+                        imageUrls.add(RELATIVE_UPLOAD_DIR + "/" + fileName); // 리스트에 파일 상대주소 추가
+                    } catch (IOException e) {
+                        e.printStackTrace(); // 에러 발생시 콘솔에 출력
+                    }
                 }
             }
         }
+
         request_ToCenter.setImageUrls(imageUrls); // 이미지 주소들은 시설물 객체에 저장
 
         return request_ToCenterRepository.save(request_ToCenter);
