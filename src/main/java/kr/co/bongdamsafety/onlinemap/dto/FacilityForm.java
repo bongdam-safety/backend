bcc67010643f14b97c52a0c2b9cd3a368ead1199
mgkim1 <mgkim1209@outlook.com>
2024-11-18 18:02:46 +0900

    기존에 Account로 작성되어 있던 것을 User로 변경

--- src/main/java/kr/co/bongdamsafety/onlinemap/dto/FacilityForm.java
+++ src/main/java/kr/co/bongdamsafety/onlinemap/dto/FacilityForm.java
@@ -1,10 +1,10 @@
 package kr.co.bongdamsafety.onlinemap.dto;
 
-import kr.co.bongdamsafety.onlinemap.entity.Account;
 import kr.co.bongdamsafety.onlinemap.entity.Facility;
 import kr.co.bongdamsafety.onlinemap.entity.FacilityCategory;
-import kr.co.bongdamsafety.onlinemap.repository.AccountRepository;
+import kr.co.bongdamsafety.onlinemap.entity.User;
 import kr.co.bongdamsafety.onlinemap.repository.FacilityCategoryRepository;
+import kr.co.bongdamsafety.onlinemap.repository.UserRepository;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,27 +30,27 @@ public class FacilityForm {
     private Timestamp date_requested_edit; // 정보수정 요청일
     private Timestamp date_created; // 신규요청 승인일
     private Timestamp date_edited; // 수정요청 승인일
-    private String account_id_agreed_new; // 신규요청 승인한 계정
-    private String account_id_agreed_edit; // 수정요청 승인한 계정
+    private Long account_id_agreed_new; // 신규요청 승인한 계정
+    private Long account_id_agreed_edit; // 수정요청 승인한 계정
 
 
-    public Facility toEntity(FacilityCategoryRepository facilityCategoryRepository, AccountRepository accountRepository_new, AccountRepository accountRepository_edit) {
+    public Facility toEntity(FacilityCategoryRepository facilityCategoryRepository, UserRepository userRepository_new, UserRepository userRepository_edit) {
         // DTO(데이터 전송 객체)를 Entity(데이터베이스 객체)로 변환
         // 매개변수 : 시설물 분류 조회를 위한 리포지토리, 생성계정 조회를 위한 리포지토리, 수정계정 조회를 위한 리포지토리
         FacilityCategory facilityCategory = facilityCategoryRepository.findById(facilityCategoryId)
                 .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 시설물 분류번호입니다: " + facilityCategoryId));
         // id를 통해 시설물 찾기. 만약 없을 경우 예외처리
 
-        Account account_agreed_new = null;
+        User account_agreed_new = null;
         if (account_id_agreed_new != null) { // 신규장소추가요청 승인한 계정이 있을 경우
-            account_agreed_new = accountRepository_new.findById(account_id_agreed_new)
+            account_agreed_new = userRepository_new.findById(account_id_agreed_new)
                     .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 생성계정입니다: " + account_id_agreed_new));
             // id를 통해 계정 찾기. 만약 없을 경우 예외처리
         }
 
-        Account account_agreed_edit = null;
+        User account_agreed_edit = null;
         if (account_id_agreed_edit != null) { // 수정요청 승인한 계정이 있을 경우
-            account_agreed_edit = accountRepository_edit.findById(account_id_agreed_edit)
+            account_agreed_edit = userRepository_edit.findById(account_id_agreed_edit)
                     .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 수정계정입니다: " + account_id_agreed_edit));
             // id를 통해 계정 찾기. 만약 없을 경우 예외처리
         }
