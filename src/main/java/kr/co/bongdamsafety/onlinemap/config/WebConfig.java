a908facfe457fed4e44c11f8f6ebe73d9f025f3b
김명규 <mgkim1209@outlook.com>
2024-11-19 23:42:15 +0900

    cors가 필요하지 않아 삭제

--- src/main/java/kr/co/bongdamsafety/onlinemap/config/WebConfig.java
+++ src/main/java/kr/co/bongdamsafety/onlinemap/config/WebConfig.java
@@ -1,28 +1,11 @@
 package kr.co.bongdamsafety.onlinemap.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost", "http://bongdamsafe.co.kr:3000", "http://bongdamsafe.co.kr")
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-
-            }
-        };
-    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
