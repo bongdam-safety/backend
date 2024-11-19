b1ba97d6a45e7776498d8ff3bdcfcefe2d3ebb0f
mgkim1 <mgkim1209@outlook.com>
2024-11-19 19:31:51 +0900

    설명(주석) 추가

--- src/main/java/kr/co/bongdamsafety/onlinemap/config/SecurityConfig.java
+++ src/main/java/kr/co/bongdamsafety/onlinemap/config/SecurityConfig.java
@@ -23,18 +23,23 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                // 정보조회 및 요청 관련 기능
                                 "/",
                                 "/api/facility",
                                 "/api/facility/{id}",
                                 "/api/facility/category/{id}",
                                 "/api/facilityCategory",
                                 "/api/facilityCategory/{id}",
-                                "/api/users/register",
-                                "/api/users/login",
-                                "/api/upload",
-                                "/api/send-sms",
                                 "/api/request_Edit",
                                 "/api/request_ToCenter",
+                                // 계정 관련 기능
+                                "/api/users/register",
+                                "/api/users/login",
+                                // 이미지인식 및 문자전송 관련 기능
+                                "/api/upload",
+                                "/api/send-sms",
+
+                                // 임시로, 로그인 없이도 모든 api에 접근 가능하도록 설정
                                 "/api/**"
                         ).permitAll()
                         .anyRequest().authenticated()
