e80767af871434cbf6724132b3cb736c6590f9f2
김명규 <mgkim1209@outlook.com>
2025-03-01 23:01:47 +0900

    경고 해결: Not annotated method overrides method annotated with @NonNullApi
    경고 해결: Not annotated method overrides method annotated with @NonNullApi

--- src/main/java/kr/co/bongdamsafety/onlinemap/repository/FacilityCategoryRepository.java
+++ src/main/java/kr/co/bongdamsafety/onlinemap/repository/FacilityCategoryRepository.java
@@ -2,10 +2,12 @@ package kr.co.bongdamsafety.onlinemap.repository;
 
 import kr.co.bongdamsafety.onlinemap.entity.FacilityCategory;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.ArrayList;
 
 public interface FacilityCategoryRepository extends CrudRepository<FacilityCategory, Long> { // 시설물유형 레포지토리
     @Override
+    @NonNull
     ArrayList<FacilityCategory> findAll(); // 오버라이딩 안하면 원래는 Iterable
 }
--- src/main/java/kr/co/bongdamsafety/onlinemap/repository/FacilityRepository.java
+++ src/main/java/kr/co/bongdamsafety/onlinemap/repository/FacilityRepository.java
@@ -3,11 +3,13 @@ package kr.co.bongdamsafety.onlinemap.repository;
 import kr.co.bongdamsafety.onlinemap.entity.Facility;
 import kr.co.bongdamsafety.onlinemap.entity.FacilityCategory;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.ArrayList;
 
 public interface FacilityRepository extends CrudRepository<Facility, Long> { // 시설물 레포지토리
     @Override
+    @NonNull
     ArrayList<Facility> findAll(); // 오버라이딩 안하면 원래는 Iterable
     ArrayList<Facility> findByFacilityCategory(FacilityCategory facilityCategory);
 
